c452782313405c5a56519b206b02b7596bd3a48e
Piyush <bhangalepiyush@gmail.com>
2023-08-09 22:17:09 +0530

    feat(style): add unocss

--- src/root.tsx
+++ src/root.tsx
@@ -6,6 +6,7 @@ import {
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
+import "uno.css";
 import "./global.css";
 
 export default component$(() => {
