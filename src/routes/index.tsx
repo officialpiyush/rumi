beac8c0f52686a845c8271a5fdcde621dc434e56
Piyush <bhangalepiyush@gmail.com>
2023-08-09 22:43:30 +0530

    feat: create and join room button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,28 @@ export default component$(() => {
     <div class="h-full w-full flex flex-col items-center justify-center gap-4">
       <h1 class="text-6xl italic font-bold tracking-widest">Rumi~</h1>
       <h2 class="text-md text-zinc-700">smol rooms for smol chats</h2>
+
+      <div class="py-4 flex flex-col gap-4 items-center">
+        <input
+          class="bg-black border-12 py-2 px-4 placeholder:(text-slate-600) border-double"
+          type="text"
+          placeholder="enter room name~"
+        />
+
+        <div class="flex items-center gap-2">
+          <button>
+            <div class="bg-black border-12 py-2 px-4 border-double border-blue hover:(border-black bg-blue text-black)">
+              create room
+            </div>
+          </button>
+
+          <button>
+            <div class="bg-black border-12 py-2 px-4 border-double border-teal hover:(border-black bg-teal text-black)">
+              join room
+            </div>
+          </button>
+        </div>
+      </div>
     </div>
   );
 });
